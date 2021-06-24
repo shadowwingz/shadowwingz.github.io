201ace0c492df10a69f87aad9563dd4a8f5c767a
shadowwingz <shadowwingzhang@gmail.com>
2021-06-24 23:51:08 +0800

    add music list

---
+++ vue/player/player.js
@@ -0,0 +1,18 @@
+var app = new Vue({
+    el: "#player",
+    data: {
+        query: "",
+        musicList: []
+    },
+    methods: {
+        searchMusic: function () {
+            var that = this;
+            axios.get("https://autumnfish.cn/search?keywords=" + this.query)
+                .then(function (response) {
+                    that.musicList = response.data.result.songs;
+                    console.log(response)
+                }, function (err) {
+                })
+        }
+    }
+})
