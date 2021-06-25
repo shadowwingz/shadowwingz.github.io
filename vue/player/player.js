ce398a48b54c49cfc0b9470d7143c82c3bc8efbc
shadowwingz <shadowwingzhang@gmail.com>
2021-06-25 23:44:51 +0800

    add play music

--- vue/player/player.js
+++ vue/player/player.js
@@ -2,17 +2,28 @@ var app = new Vue({
     el: "#player",
     data: {
         query: "",
-        musicList: []
+        musicList: [],
+        musicUrl: "",
     },
     methods: {
         searchMusic: function () {
-            var that = this;
+            const that = this;
             axios.get("https://autumnfish.cn/search?keywords=" + this.query)
                 .then(function (response) {
                     that.musicList = response.data.result.songs;
                     console.log(response)
                 }, function (err) {
+                    console.log(err);
                 })
+        },
+        playMusic: function (musicId) {
+            const that = this;
+            axios.get("https://autumnfish.cn/song/url?id=" + musicId)
+                .then(function (response) {
+                    that.musicUrl = response.data.data[0].url;
+                }, function (err) {
+                    console.log(err);
+                });
         }
     }
 })
